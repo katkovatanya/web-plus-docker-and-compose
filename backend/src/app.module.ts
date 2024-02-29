780308353f0f1fc47a7030797d5e098edb9604f0
Tanya Katkova <yndx-katkovatanya@yandex.ru>
2024-02-29 23:11:52 +0300

    fix: host

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -22,7 +22,7 @@ import { WishPartial } from './wishes/entities/wish-partial.entity';
     AuthModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: '158.160.149.44',
+      host: 'postgres',
       port: 5432,
       username: 'student',
       password: 'student',
